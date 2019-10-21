843c0cc25dde9ac0a4fb9990006425825dfedc8f
Mellbrother <keisuke0907@outlook.jp>
2019-10-21 12:14:07 +0900

    登録者画面に登録者の順位を変更するボタンを追加:

--- app/controllers/registered_students_controller.rb
+++ app/controllers/registered_students_controller.rb
@@ -1,4 +1,6 @@
 class RegisteredStudentsController < ApplicationController
+  before_action :remove_null_registered_students, only: [:register]
+
   def index
   	@students = Student.all
   	@registered_student_ids = RegisteredStudent.all.map{|rs| rs.student_id}
@@ -9,15 +11,17 @@ class RegisteredStudentsController < ApplicationController
   end
 
   def register
+  	RegisteredStudent.all.sort{ |a,b| a.rank <=> b.rank }.each.with_index(1) do |rs, i|
+  		next if rs.rank == i
+  		rs.rank = i
+  		rs.save!
+  	end
   	@registered_students = RegisteredStudent.all
-  	@students = @registered_students.map{|rs|
-  		Student.find_by(id: rs.student_id)
-  	}.compact
-
   end
 
   def create
   	registered_student = RegisteredStudent.new(registed_student_params)
+  	registered_student.rank = RegisteredStudent.count + 1
   	registered_student.save!
   	redirect_to registered_students_path
   end
@@ -29,15 +33,42 @@ class RegisteredStudentsController < ApplicationController
   end
 
   def rank_up
-  	registered_student = RegisteredStudent.find(params[:id])
+  	registered_student = RegisteredStudent.find_by(student_id: params[:id])
+  	unless registered_student.rank == 1
+	  	target = RegisteredStudent.find_by(rank: (registered_student.rank - 1))
+	  	tmp = registered_student.rank
+	  	registered_student.update(rank: target.rank)
+	  	target.update(rank: tmp)
+	end
+	redirect_to registered_students_register_path
   end
 
   def rank_down
+  	registered_student = RegisteredStudent.find_by(student_id: params[:id])
+  	unless registered_student.rank == RegisteredStudent.count
+	  	target = RegisteredStudent.find_by(rank: (registered_student.rank + 1))
+	  	tmp = registered_student.rank
+	  	tmp = registered_student.rank
+	  	registered_student.update(rank: target.rank)
+	  	target.update(rank: tmp)
+	end
+	redirect_to registered_students_register_path
   end	
 
   private
 
   def registed_student_params
   	params.require(:registered_student).permit(:student_id)
+  end
+
+  def remove_null_registered_students
+  	@students = RegisteredStudent.all.map{|rs|
+  		sc = Student.find_by(id: rs.student_id)
+  		unless sc
+  			rs.delete
+  			return nil
+  		end
+  		sc
+  	}.compact
   end
 end
