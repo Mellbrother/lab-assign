bc51793197c83ca3ef3a920dfff0c3ce82dfe1c4
Mellbrother <keisuke0907@outlook.jp>
2019-10-21 17:42:51 +0900

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+	tmp = Faker::Name.name
+	User.create(
+		name: tmp,
+		email: "teacher#{i}@example.com",
+		admin: false,
+		teacher: true,
+		password: "password",
+		password_confirmation: "password"
+	)
+	Teacher.create(
+		mynumber: 157000 + i,
+		name: tmp
+	)
+end
+
+100.times do |i|
+	tmp = Faker::Name.name
+	User.create(
+		name: tmp,
+		email: "student#{i}@example.com",
+		admin: false,
+		student: true,
+		password: "password",
+		password_confirmation: "password"
+	)
+	Student.create(
+		mynumber: 155000 + i,
+		name: tmp,
+		gpa: rand(1.5..3.9).floor(2),
+		gp: rand(100..400)
+	)
+end	
+User.create(
+		name: "admin",
+		email: "admin@example.com",
+		admin: true,
+		password: "password",
+		password_confirmation: "password"
+)
